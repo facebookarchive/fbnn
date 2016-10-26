538ccf101db08ff5f1641a924353e7771aa90da6
David Grangier <grangier@fb.com>
2016-10-26 11:39:43 -0700

    UnfoldedTemporalConvolution supports inplace padding
    Summary: like cudnn the constructor now accept padding option with symmetric padding.
    
    Reviewed By: jgehring
    
    Differential Revision: D4078798
    
    fbshipit-source-id: ad583e54398243f9559bf21b946b88bd1110bfd6

--- fbnn/UnfoldedTemporalConvolution.lua
+++ fbnn/UnfoldedTemporalConvolution.lua
@@ -1,6 +1,6 @@
 local UTC, parent = torch.class('nn.UnfoldedTemporalConvolution', 'nn.Module')
 
-function UTC:__init(nin, nout, kw, dW)
+function UTC:__init(nin, nout, kw, dW, pad)
     dW = dW or 1
     assert(dW == 1, "nn.UnfoldedTemporalConvolution only supports dW = 1")
 
@@ -10,6 +10,7 @@ function UTC:__init(nin, nout, kw, dW)
     -- sizes
     self.kW = kw
     self.dW = 1
+    self.pad = pad or 0
     self.inputFrameSize = nin
     self.outputFrameSize = nout
 
@@ -20,7 +21,8 @@ function UTC:__init(nin, nout, kw, dW)
     self.gradBias = self.linear.gradBias
 
     -- internal buffer
-    self._input = torch.Tensor()
+    self._paddedInput = torch.Tensor()
+    self._paddedGradInput = torch.Tensor()
     self._unfoldedInput = torch.Tensor()
     self._linearGradOutput = torch.Tensor()
     self._unfoldedGradInput = torch.Tensor()
@@ -40,7 +42,7 @@ end
 function UTC:updateOutput(input)
     local nout = self.outputFrameSize
     local nin  = self.inputFrameSize
-    local padl = self.kW - 1
+    local kpad = self.kW - 1    -- pad added between sentence in batch
     local bsz, l = input:size(1), input:size(2)
 
     -- no batch?
@@ -54,17 +56,18 @@ function UTC:updateOutput(input)
     -- pad
     -- this a bit complicated but
     -- we want padding at beginning, end and between examples = (bsz + 1) pads
-    local n = (bsz + 1) * padl + bsz * l
-    self._input
+    local padl = l + 2 * self.pad           -- padded length
+    local n = (bsz + 1) * kpad + bsz * padl -- add kpad between each sample
+    self._paddedInput
         :resize(n, nin)
         :zero()
-        :narrow(1, padl + 1, bsz * (l + padl))
+        :narrow(1, kpad + 1, bsz * (padl + kpad))
         :view(bsz, -1, nin)
-        :narrow(2, 1, l)
+        :narrow(2, 1 + self.pad, l)
         :copy(input)
 
     -- unfold
-    local uinput = self._input
+    local uinput = self._paddedInput
         :view(-1)
         :unfold(1, nin * self.kW, nin)
     self._unfoldedInput
@@ -75,7 +78,7 @@ function UTC:updateOutput(input)
     local loutput = self.linear:updateOutput(self._unfoldedInput)
     self.output = loutput
         :view(bsz, -1, nout)
-        :narrow(2, padl + 1, l - padl)
+        :narrow(2, kpad + 1, padl - kpad)
 
     return self.output
 end
@@ -83,8 +86,9 @@ end
 function UTC:updateGradInput(input, gradOutput)
     local nout = self.outputFrameSize
     local nin  = self.inputFrameSize
-    local padl = self.kW - 1
+    local kpad = self.kW - 1
     local bsz, l = input:size(1), input:size(2)
+    local padl = l + 2*self.pad
 
     -- no batch ?
     if input:dim() == 2 then
@@ -101,7 +105,7 @@ function UTC:updateGradInput(input, gradOutput)
         :resizeAs(self.linear.output)
         :zero()
         :view(bsz, -1, nout)
-        :narrow(2, padl + 1, l - padl)
+        :narrow(2, kpad + 1, padl - kpad)
         :copy(gradOutput)
     self.linear
         :updateGradInput(self._unfoldedInput, self._linearGradOutput)
@@ -109,19 +113,22 @@ function UTC:updateGradInput(input, gradOutput)
     -- reduce
     self._unfoldedGradInput:set(
         self.linear.gradInput:storage(),
-        nin * (self.kW - 1) + 1,     -- offset
-        bsz,                         -- sz1
-        nin * self.kW * (l + padl),  -- st1
-        l,                           -- sz2
-        nin * self.kW,               -- st2
-        self.kW,                     -- sz3
-        nin *(self.kW - 1),          -- st3
-        nin,                         -- sz4
-        1)                           -- st4
+        nin * (self.kW - 1) + 1,        -- offset
+        bsz,                            -- sz1
+        nin * self.kW * (padl + kpad),  -- st1
+        padl,                           -- sz2
+        nin * self.kW,                  -- st2
+        self.kW,                        -- sz3
+        nin *(self.kW - 1),             -- st3
+        nin,                            -- sz4
+        1)                              -- st4
 
-    self.gradInput = self.gradInput
+    self._paddedGradInput = self._paddedGradInput
         :sum(self._unfoldedGradInput, 3)
         :select(3, 1)
+
+    self.gradInput = self._paddedGradInput
+        :narrow(2, 1 + self.pad, l)
 
     return self.gradInput
 end
@@ -141,44 +148,50 @@ function UTC:test()
         return  delta < epsilon, delta
     end
 
-    local function checkforward()
-        local input = torch.randn(16, 25, 5)
-        local conv = nn.TemporalConvolution(5, 10, 3)
-        conv:forward(input)
+    local function checkforwardbackward(bsz, l, nin, nout, kw, pad, type, eps)
+        bsz = bsz or 16
+        l = l or 25
+        nin = nin or 5
+        nout = nout or 10
+        kw = kw or 3
+        type    = type or 'torch.DoubleTensor'
+        local epsilon = eps or 1e-12
 
-        local utc = nn.UnfoldedTemporalConvolution(5, 10, 3)
-        utc.weight:copy(conv.weight)
-        utc.bias:copy(conv.bias)
-        utc:forward(input)
-        assert(tensoreq(conv.output, utc.output))
-    end
-
-    local function checkforwardbackward(bsz, l, nin, nout, kw, type, epsilon)
-        local type    = type or 'torch.DoubleTensor'
-        local epsilon = epsilon or 1e-12
+        -- random input
         local input = torch.randn(bsz, l, nin):type(type)
 
-        local conv = nn.TemporalConvolution(nin, nout, kw):type(type)
+        -- torch reference implementation
+        local conv = nn.TemporalConvolution(nin, nout, kw, 1)
+        local nopad = conv
+        if pad then
+            conv = nn.Sequential()
+                :add(nn.Padding(2, -pad))
+                :add(nn.Padding(2, pad))
+                :add(conv)
+        end
+        conv:type(type)
         conv:forward(input)
         conv:zeroGradParameters()
         local gout = torch.randn(conv.output:size()):type(type)
         conv:backward(input, gout)
 
-        local utc = nn.UnfoldedTemporalConvolution(nin, nout, kw):type(type)
-        utc.weight:copy(conv.weight)
-        utc.bias:copy(conv.bias)
+        -- our implementation
+        local utc = nn.UnfoldedTemporalConvolution(nin, nout, kw, 1, pad)
+        utc:type(type)
+        utc.weight:copy(nopad.weight)
+        utc.bias:copy(nopad.bias)
         utc:forward(input)
         utc:zeroGradParameters()
         utc:backward(input, gout)
 
+        -- check reference and ours have same outputs, gradients
         assert(tensoreq(conv.output, utc.output, epsilon))
         assert(tensoreq(conv.gradInput, utc.gradInput, epsilon))
-        assert(tensoreq(conv.gradWeight, utc.gradWeight, epsilon))
-        assert(tensoreq(conv.gradBias, utc.gradBias, epsilon))
+        assert(tensoreq(nopad.gradWeight, utc.gradWeight, epsilon))
+        assert(tensoreq(nopad.gradBias, utc.gradBias, epsilon))
     end
 
     return {
-        checkforward = checkforward,
         checkforwardbackward = checkforwardbackward,
     }
 end
